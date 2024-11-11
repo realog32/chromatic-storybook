bc850e70a551eb81355d1ba02a3a8c311a699778
Ogom Okafor <ogomokafor@gmail.com>
2024-11-11 15:55:53 -0600

    Add standalone ButtonComponent with template and unit tests

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { KENDO_BUTTONS } from '@progress/kendo-angular-buttons';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [KENDO_BUTTONS],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  disabled = Input();
+}
